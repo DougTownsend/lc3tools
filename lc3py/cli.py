28298519031d696c85f68c23a317c3b6e6eadcbc
Doug Townsend <townsend.doug@gmail.com>
2026-04-15 17:47:51 -0400

    fixed error message when qt fails

--- lc3py/cli.py
+++ lc3py/cli.py
@@ -13,7 +13,15 @@ def lc3pysim():
     try:
         from .cli_bindings import curs_main
     except ImportError:
-        print("lc3pysim is not available on Windows. Use lc3sim instead.",
+        print("Error: lc3pysim requires Qt to be installed.",
               file=sys.stderr)
+        if sys.platform == "darwin":
+            print("  Install it with: brew install qt pkg-config", file=sys.stderr)
+            print("  Then rebuild:    pip3 install . --no-build-isolation --no-cache-dir", file=sys.stderr)
+        elif sys.platform == "win32":
+            print("  On Windows, use the MSI installer instead of pip.", file=sys.stderr)
+        else:
+            print("  Ubuntu/Debian: sudo apt install libqt5widgets5 qtbase5-dev", file=sys.stderr)
+            print("  Then rebuild:  pip3 install . --no-build-isolation --no-cache-dir", file=sys.stderr)
         sys.exit(1)
     sys.exit(curs_main(sys.argv, sys.executable))
